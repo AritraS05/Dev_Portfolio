0b170b9b85bb2d26ef7cb005276f6c2a543f9283
Aritra <aritrasarkar00007@gmail.com>
2025-03-07 21:42:57 +0530

    error resolved

--- components/animated/animated-image.tsx
+++ components/animated/animated-image.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { cn } from "@/lib/utils";
+import { cn } from "@/utils/cn";
 import { motion } from "motion/react";
 import Image, { ImageProps } from "next/image";
 import { HTMLAttributes } from "react";
